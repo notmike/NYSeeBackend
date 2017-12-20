d83b246f003bd20767d43e42336f54dbc884b636
mike <notmike@users.noreply.github.com>
2017-12-20 16:56:38 -0500

    updated for path arrays instead of strings

--- src/models/pathModel.js
+++ src/models/pathModel.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 export const PathSchema = new Schema({
     _id: { type: String, required: 'Must have a PathId' },
     pathArray: [{
-        path: String,
+        path: [],
         rating: {
             type: Number,
             default: 0
