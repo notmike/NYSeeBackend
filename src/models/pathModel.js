b3dda8b3554601c5cc63b7aa5d742702a0b5ca21
mike <notmike@users.noreply.github.com>
2017-12-20 18:16:26 -0500

    trying to get frontend to talk to backend

--- src/models/pathModel.js
+++ src/models/pathModel.js
@@ -2,10 +2,19 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+// var SegmentSchema = new Schema({ 
+//     direction : String,
+//     steps : Number
+// });
+
+// var TestSchema = new Schema({
+//     num1 : Number,
+//     num2 : Number, });
+
 export const PathSchema = new Schema({
     _id: { type: String, required: 'Must have a PathId' },
     pathArray: [{
-        path: [],
+        path: Object,
         rating: {
             type: Number,
             default: 0
